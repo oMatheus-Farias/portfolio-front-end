9bbba322a69e0ffed0f9afc0e2ecc88fb542fc53
Matheus <matheusfariasdasneves@gmail.com>
2024-07-21 16:55:45 -0300

    chore: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,24 @@ export const theme = {
     fontFamily: {
       sans: ["Montserrat", "sans-serif"],
     },
+    colors: {
+      primary: "#01001A",
+      secondary: "#4213FF",
+      white: "#FFFFFF",
+      black: "#000000",
+      purpleClean: "#3B335C",
+      purpleLogoLogin: "#080622",
+      bgDark: "#010010",
+      green: "#419453",
+      gray: "#888888",
+    },
+    backgroundImage: {
+      "bg-violet-vertical":
+        "linear-gradient(180deg, #7900B2 0%, #4D53A1 38%, #338496 61%, #00C8F3 100%)",
+      "bg-violet-horizontal":
+        "linear-gradient(90deg, #7900B2 0%, #4D53A1 38%, #338496 61%, #00C8F3 100%)",
+      "bg-purple-vertical": "linear-gradient(180deg, #0B002B 0%, #070023 100%)",
+    },
   },
 }
 import tailwindcssAnimate from "tailwindcss-animate"
