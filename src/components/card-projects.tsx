00ae7e28edf977fa05a145ad2e1c706708da5884
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2024-07-23 17:22:55 -0300

    feat: add CardProjects with initial config

---
+++ src/components/card-projects.tsx
@@ -0,0 +1,13 @@
+const CardProjects = () => {
+  return (
+    <div className="h-[350px] w-[550px] overflow-hidden rounded-[10px] border-[3px] border-secondary grayscale transition-all duration-200 ease-linear hover:scale-[1.02] hover:grayscale-0">
+      <img
+        src="/fsw-store-cover-test.jpg"
+        alt="Project 01"
+        className="h-full w-full object-cover"
+      />
+    </div>
+  )
+}
+
+export { CardProjects }
