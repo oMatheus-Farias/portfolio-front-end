5cbda40c33354f7f1c9e9403f5b4133ac6b26c13
Matheus <matheusfariasdasneves@gmail.com>
2024-07-22 21:32:46 -0300

    feat: add constant of technologies and images

---
+++ src/constants/technologies.ts
@@ -0,0 +1,43 @@
+//FIXME: Add technologies
+export const TECHNOLOGIES = [
+  {
+    id: 1,
+    name: "React",
+    img: "/react.png",
+  },
+  {
+    id: 2,
+    name: "Next.js",
+    img: "/nextjs.png",
+  },
+  {
+    id: 3,
+    name: "TailwindCSS",
+    img: "/tailwindcss.png",
+  },
+  {
+    id: 4,
+    name: "JavaScript",
+    img: "/javascript.png",
+  },
+  {
+    id: 5,
+    name: "TypeScript",
+    img: "/typescript.png",
+  },
+  {
+    id: 6,
+    name: "Node.js",
+    img: "/nodejs.png",
+  },
+  {
+    id: 7,
+    name: "Prisma",
+    img: "/prisma.png",
+  },
+  {
+    id: 8,
+    name: "Git",
+    img: "/git.png",
+  },
+]
