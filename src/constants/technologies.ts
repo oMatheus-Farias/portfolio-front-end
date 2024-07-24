bcf3fa0ab737fec2cb211e7685fb391ac79020b4
Matheus <matheusfariasdasneves@gmail.com>
2024-07-24 15:15:30 -0300

    feat: add technologies

--- src/constants/technologies.ts
+++ src/constants/technologies.ts
@@ -1,43 +1,77 @@
-//FIXME: Add technologies
 export const TECHNOLOGIES = [
   {
-    id: 1,
+    id: "1",
     name: "React",
     img: "/react.png",
   },
   {
-    id: 2,
+    id: "2",
     name: "Next.js",
     img: "/nextjs.png",
   },
   {
-    id: 3,
-    name: "TailwindCSS",
-    img: "/tailwindcss.png",
-  },
-  {
-    id: 4,
+    id: "3",
     name: "JavaScript",
     img: "/javascript.png",
   },
   {
-    id: 5,
+    id: "4",
     name: "TypeScript",
     img: "/typescript.png",
   },
   {
-    id: 6,
-    name: "Node.js",
-    img: "/nodejs.png",
+    id: "5",
+    name: "HTML",
+    img: "/html.png",
   },
   {
-    id: 7,
-    name: "Prisma",
-    img: "/prisma.png",
+    id: "6",
+    name: "CSS",
+    img: "/css.png",
   },
   {
-    id: 8,
+    id: "7",
+    name: "TailwindCSS",
+    img: "/tailwindcss.png",
+  },
+  {
+    id: "8",
+    name: "Styled C...",
+    img: "/styled-components.png",
+  },
+  {
+    id: "9",
+    name: "Sass",
+    img: "/sass.png",
+  },
+  {
+    id: "10",
+    name: "Jest",
+    img: "/jest.png",
+  },
+  {
+    id: "11",
+    name: "PostgreSQL",
+    img: "/postgresql.png",
+  },
+  {
+    id: "12",
     name: "Git",
     img: "/git.png",
+  },
+  {
+    id: "13",
+    name: "GitHub",
+    img: "/github.png",
+  },
+  {
+    id: "14",
+    name: "Docker",
+    img: "/docker.png",
+  },
+  {
+    id: "15",
+    name: "Figma",
+    img: "/figma.png",
   },
 ]
