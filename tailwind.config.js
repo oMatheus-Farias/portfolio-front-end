8ef1c2e895a9d0b61798bb0ecc5cc713174d0439
Matheus <matheusfariasdasneves@gmail.com>
2024-07-24 09:51:01 -0300

    feat: add background-01 mobile and xl

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,13 @@ export const theme = {
       "purple-vertical": "linear-gradient(180deg, #0B002B 0%, #070023 100%)",
       "me-img-mobile": "url('/me-img-mobile.png')",
       "me-img-desktop": "url('/me-img-desktop.png')",
+      "background-01-mobile": "url('/background-01-mobile.png')",
+      "background-01": "url('/background-01.png')",
+      "background-01-xl": "url('/background-01-xl.png')",
+      "background-02": "url('/background-02.png')",
+    },
+    screens: {
+      "0xl": "1374px",
     },
   },
 }
