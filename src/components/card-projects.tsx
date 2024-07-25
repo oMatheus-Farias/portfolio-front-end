868f43c0130efc612b876430ba0d2f21f82ce5c8
Matheus <matheusfariasdasneves@gmail.com>
2024-07-25 20:36:04 -0300

    refactor: ensure in mobile, card projects do not have grayscale

--- src/components/card-projects.tsx
+++ src/components/card-projects.tsx
@@ -5,7 +5,7 @@ interface CardProjectsProps {
 
 const CardProjects = ({ image, name }: CardProjectsProps) => {
   return (
-    <div className="relative h-[280px] w-[390px] overflow-hidden rounded-[10px] border-[3px] border-secondary grayscale transition-all duration-200 ease-linear hover:scale-[1.02] hover:grayscale-0 lg:h-[350px] lg:w-[550px]">
+    <div className="relative h-[280px] w-[390px] overflow-hidden rounded-[10px] border-[3px] border-secondary transition-all duration-200 ease-linear hover:scale-[1.02] hover:grayscale-0 lg:h-[350px] lg:w-[550px] lg:grayscale">
       <div className="absolute bottom-4 left-4">
         <h4 className="bg-violet-horizontal bg-clip-text text-2xl font-extrabold text-transparent lg:text-3xl">
           {name}
