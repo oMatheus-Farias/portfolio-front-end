252c19d2f9dc8f5be1a5aac62ed90b17483328a2
Matheus <matheusfariasdasneves@gmail.com>
2024-07-28 12:57:45 -0300

    feat: add Sidebar component

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,25 @@
+import { LayoutGrid } from "lucide-react"
+
+import { AvatarSidebar } from "./avatar-sidebar"
+import { Separator } from "./ui/separator"
+
+const Sidebar = () => {
+  return (
+    <aside className="h-full w-full bg-violet-vertical pr-[2px]">
+      <div className="flex h-full w-full flex-col items-center bg-bgDark px-8">
+        <div className="mt-16 flex w-full flex-col gap-14">
+          <AvatarSidebar />
+
+          <Separator orientation="horizontal" className="w-full bg-white" />
+
+          <div className="flex w-full cursor-pointer gap-2 rounded bg-purpleClean px-6 py-2 transition-all duration-200 ease-linear hover:bg-opacity-80">
+            <LayoutGrid color="#FFF" />
+            <span className="font-semibold text-white">Projetos</span>
+          </div>
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export { Sidebar }
--- src/pages/admin-panel.tsx
+++ src/pages/admin-panel.tsx
@@ -1,7 +1,15 @@
+import { Sidebar } from "@/components/sidebar"
+
 const AdminPanel = () => {
   return (
-    <div>
-      <h1 className="text-white">admin panel</h1>
+    <div className="flex min-h-screen w-full">
+      <div className="hidden min-h-full w-72 lg:block">
+        <Sidebar />
+      </div>
+
+      <section>
+        <h1 className="text-white">Admin Panel</h1>
+      </section>
     </div>
   )
 }
