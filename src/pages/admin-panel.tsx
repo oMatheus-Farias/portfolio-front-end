293a928ef96768edf744ea4b0cb7d769c9707002
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2024-07-29 11:34:03 -0300

    feat: ensure that the number of projects is shown and render the register button

--- src/pages/admin-panel.tsx
+++ src/pages/admin-panel.tsx
@@ -1,5 +1,8 @@
+import { Plus } from "lucide-react"
+
 import { HeaderAdminPanel } from "@/components/header-admin-panel"
 import { Sidebar } from "@/components/sidebar"
+import { Button } from "@/components/ui/button"
 
 const AdminPanel = () => {
   return (
@@ -8,11 +11,24 @@ const AdminPanel = () => {
         <Sidebar />
       </div>
 
-      <section className="w-full">
+      <div className="w-full">
         <div className="h-[4.375rem] w-full">
           <HeaderAdminPanel />
         </div>
-      </section>
+
+        <section className="mt-20 w-full px-10">
+          <div className="flex w-full items-center justify-between">
+            <span className="font-semibold text-white">
+              Projetos cadastrados: 3
+            </span>
+
+            <Button className="flex items-center justify-center gap-2 rounded-full bg-purpleClean px-5 py-5 text-lg font-medium text-white shadow-md transition-all duration-200 ease-linear hover:bg-secondary">
+              <Plus size={16} />
+              Cadastrar
+            </Button>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
