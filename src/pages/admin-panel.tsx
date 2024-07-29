a7d6784a7df5a09dbe7fc25862174272b9f6b989
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2024-07-29 11:54:51 -0300

    feat: add project search through API

--- src/pages/admin-panel.tsx
+++ src/pages/admin-panel.tsx
@@ -1,10 +1,33 @@
+import axios from "axios"
 import { Plus } from "lucide-react"
+import { useQuery } from "react-query"
 
+import { IProjects } from "@/components/carousel-projects"
 import { HeaderAdminPanel } from "@/components/header-admin-panel"
 import { Sidebar } from "@/components/sidebar"
 import { Button } from "@/components/ui/button"
+import { renderProjectsLength } from "@/helpers/render-prrojects-length"
 
 const AdminPanel = () => {
+  const { data, isLoading } = useQuery<IProjects[]>(
+    "projects",
+    async () => {
+      return axios
+        .get(import.meta.env.VITE_URL, {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
+          },
+        })
+        .then((response) => response.data)
+        .catch((error) => {
+          throw new Error(error)
+        })
+    },
+    {
+      refetchOnMount: true,
+    },
+  )
+
   return (
     <div className="flex min-h-screen w-full">
       <div className="hidden min-h-full min-w-72 lg:block">
@@ -18,9 +41,7 @@ const AdminPanel = () => {
 
         <section className="mt-20 w-full px-10">
           <div className="flex w-full items-center justify-between">
-            <span className="font-semibold text-white">
-              Projetos cadastrados: 3
-            </span>
+            {renderProjectsLength({ isLoading, data })}
 
             <Button className="flex items-center justify-center gap-2 rounded-full bg-purpleClean px-5 py-5 text-lg font-medium text-white shadow-md transition-all duration-200 ease-linear hover:bg-secondary">
               <Plus size={16} />
