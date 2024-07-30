993fc529bf48c3ff9d44270d2ad216993261f5c0
Matheus <matheusfariasdasneves@gmail.com>
2024-07-30 15:40:34 -0300

    feat: add FormCreateProject

---
+++ src/components/form-create-project.tsx
@@ -0,0 +1,272 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios"
+import { LoaderCircle } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { useMutation, useQueryClient } from "react-query"
+import { z } from "zod"
+
+import { Button } from "./ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form"
+import { toast } from "./ui/use-toast"
+
+interface FormCreateProjectProps {
+  setOpenDialog: (value: boolean) => void
+}
+
+interface IProjectCreate {
+  userId: string
+  name: string | undefined
+  description: string | undefined
+  imagesUrl: string[] | []
+  repositoryUrl: string | undefined
+  projectUrl: string | undefined
+  technologies: string[] | []
+}
+
+const formSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  imagesUrl: z.string().optional(),
+  repositoryUrl: z.string().optional(),
+  projectUrl: z.string().optional(),
+  technologies: z.string().optional(),
+})
+
+type FormData = z.infer<typeof formSchema>
+
+const FormCreateProject = ({ setOpenDialog }: FormCreateProjectProps) => {
+  const queryClient = useQueryClient()
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      imagesUrl: "",
+      repositoryUrl: "",
+      projectUrl: "",
+      technologies: "",
+    },
+  })
+
+  const handleCloseDialogClick = () => {
+    setOpenDialog(false)
+    form.reset()
+  }
+
+  const mutation = useMutation({
+    mutationFn: async (data: IProjectCreate) => {
+      return axios.post(
+        import.meta.env.VITE_URL_CREATE_PROJECT,
+        { ...data },
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
+          },
+        },
+      )
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("projects")
+      handleCloseDialogClick()
+      toast({
+        variant: "default",
+        description: "Projeto cadastrado com sucesso.",
+      })
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    onError: (error: any) => {
+      if (error.response.status === 404) {
+        toast({
+          variant: "default",
+          description: "Dados incorretos.",
+        })
+        return
+      }
+
+      toast({
+        variant: "default",
+        description: "Desculpe, ocorreu um erro inesperado.",
+      })
+      return
+    },
+  })
+  const loading = mutation.isLoading
+
+  const handleCreateUserClick = (data: FormData) => {
+    const user = JSON.parse(localStorage.getItem("user") || "{}")
+    const userId = user.id
+
+    const createProjectFields = {
+      userId,
+      name: data.name,
+      description: data.description,
+      imagesUrl: data.imagesUrl?.split(", ") || [],
+      repositoryUrl: data.repositoryUrl,
+      projectUrl: data.projectUrl,
+      technologies: data.technologies?.split(", ") || [],
+    }
+
+    mutation.mutate(createProjectFields)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        className="mt-4 flex flex-col gap-4"
+        onSubmit={form.handleSubmit(handleCreateUserClick)}
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel htmlFor="name-project" className="text-sm text-white">
+                Nome do projeto
+              </FormLabel>
+              <FormControl>
+                <input
+                  {...form.register("name")}
+                  id="name-project"
+                  type="text"
+                  className="mt-2 h-10 w-full rounded border border-purpleClean bg-transparent px-3 text-white outline-none"
+                  placeholder="Nome do projeto"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="description"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel htmlFor="description" className="text-sm text-white">
+                Descrição
+              </FormLabel>
+              <FormControl>
+                <input
+                  {...form.register("description")}
+                  id="description"
+                  type="text"
+                  maxLength={255}
+                  className="mt-2 h-10 w-full rounded border border-purpleClean bg-transparent px-3 text-white outline-none"
+                  placeholder="Descrição do projeto"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="imagesUrl"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel htmlFor="images-url" className="text-sm text-white">
+                URL das imagens
+              </FormLabel>
+              <FormControl>
+                <input
+                  {...form.register("imagesUrl")}
+                  id="images-url"
+                  type="text"
+                  className="mt-2 h-10 w-full rounded border border-purpleClean bg-transparent px-3 text-white outline-none"
+                  placeholder="URL das imagens"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="repositoryUrl"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel
+                htmlFor="repository-url"
+                className="text-sm text-white"
+              >
+                URL do repositório
+              </FormLabel>
+              <FormControl>
+                <input
+                  {...form.register("repositoryUrl")}
+                  id="repository-url"
+                  type="text"
+                  className="mt-2 h-10 w-full rounded border border-purpleClean bg-transparent px-3 text-white outline-none"
+                  placeholder="URL do repositório"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="projectUrl"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel htmlFor="project-url" className="text-sm text-white">
+                URL do projeto
+              </FormLabel>
+              <FormControl>
+                <input
+                  {...form.register("projectUrl")}
+                  id="project-url"
+                  type="text"
+                  className="mt-2 h-10 w-full rounded border border-purpleClean bg-transparent px-3 text-white outline-none"
+                  placeholder="URL do projeto"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="technologies"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel htmlFor="technologies" className="text-sm text-white">
+                Tecnologias
+              </FormLabel>
+              <FormControl>
+                <input
+                  {...form.register("technologies")}
+                  id="technologies"
+                  type="text"
+                  className="mt-2 h-10 w-full rounded border border-purpleClean bg-transparent px-3 text-white outline-none"
+                  placeholder="Tecnologias utilizadas"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <div className="mt-4 flex w-full items-center justify-center gap-4">
+          <Button
+            type="button"
+            onClick={handleCloseDialogClick}
+            className="w-full bg-purpleClean text-white transition-all duration-200 ease-linear hover:bg-purpleClean hover:bg-opacity-80"
+          >
+            Cancelar
+          </Button>
+          <Button
+            type="submit"
+            className="w-full bg-green text-white transition-all duration-200 ease-linear hover:bg-green hover:bg-opacity-80"
+          >
+            {loading ? (
+              <LoaderCircle
+                color="#FFF"
+                size={18}
+                className="animate-spin duration-500"
+              />
+            ) : (
+              "Concluir"
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
+
+export { FormCreateProject }
